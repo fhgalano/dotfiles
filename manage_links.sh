9fcccfcbf315bd4e0578286892b525a2b5368cb8
gimmetheveggies <fhgalano@gmail.com>
2024-07-03 12:11:15 -0500

    added starship config, added automatic exclusiion of created links

--- manage_links.sh
+++ manage_links.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-ln -si "$PWD/alacritty" ~/.config/alacritty
-ln -si "$PWD/.tmux.conf" ~/.tmux.conf
+ln -sfv "$PWD/alacritty" ~/.config/alacritty
+ln -sfv "$PWD/.tmux.conf" ~/.tmux.conf
+ln -sfv "$PWD/starship.toml" ~/.config/starship.toml
+
+find . -type l | sed 's/\.\///g' > .gitignore
 
 exit 0
