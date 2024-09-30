a4c24a140ed838403fe71f028b8f0629b9e641bd
fidel <fhgalano@gmail.com>
2024-09-30 13:20:33 -0500

    made install script more visibly appealling, and added neofetch config setup

--- manage_links.sh
+++ manage_links.sh
@@ -1,44 +1,94 @@
 #!/bin/bash
 
+# color constants
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# functions
+create_symlink() {
+    sleep 1
+    echo -n " creating symlink .... "
+    sleep 1
+
+    if [ -f $2 ] | [ -d $2 ]
+    then
+        echo -e "${YELLOW}IGNORED${NC}"
+    else
+        echo -e "${GREEN}CREATING${NC}"
+        # link symbolic, force, verbose
+        ln -sfv $1 $2
+    fi
+}
+
 # alacritty 
+echo "== ALACRITTY =="
+echo -n " checking for alacritty install ...."
+sleep 1
 if [ $(which alacritty) ]
 then
-    ln -sfv "$PWD/alacritty" ~/.config/alacritty
+    echo -e "${GREEN}INSTALLED${NC}"
+    create_symlink "$PWD/alacritty/alacritty.toml" ~/.config/alacritty/alacritty.toml
 else
-    echo "alacritty not installed"
+    echo "${RED}NOT INSTALLED${NC}"
 fi
+sleep 1
 
 # tmux
+echo "== TMUX =="
+echo -n " checking for tmux instal .... "
+sleep 1
 if [ $(which tmux) ]
 then
-    ln -sfv "$PWD/.tmux.conf" ~/.tmux.conf
+    echo -e "${GREEN}INSTALLED${NC}"
+    create_symlink "$PWD/.tmux.conf" ~/.tmux.conf
 else
-    echo "tmux not installed"
+    echo -e "${RED}NOT INSTALLED${NC}"
 fi
 
 # starship
+echo "== STARSHIP =="
+echo -n " checking for starship install .... "
+sleep 1
 if [ $(which starship) ]
 then
-    ln -sfv "$PWD/starship.toml" ~/.config/starship.toml
+    echo -e "${GREEN}INSTALLED${NC}"
+    create_symlink "$PWD/starship.toml" ~/.config/starship.toml
 
-    if [ $SHELL = "/bin/bash" ]
+    echo -n " checking default shell .... "
+    if [[ $SHELL == *"bash"* ]] 
     then
-        echo "default shell is bash"
-        echo -n "  checking config..."
+        echo -e "${GREEN}BASH${NC}"
+        echo -n " checking config.... "
         grep starship ~/.bashrc >> /dev/null
         if [ $? -gt 0 ]
         then
             printf "eval \"\$(starship init bash)\"" >> ~/.bashrc
-            echo "added starship to .bashrc"
+            echo -e "${GREEN}added starship to .bashrc${NC}"
         else
-            echo "Ok"
+            echo -e "${YELLOW}STARTUP COMMAND ALREADY INSTALLED${NC}"
         fi
     fi
 else
-    echo "starship not installed"
+    echo -e "${RED}NOT INSTALLED${NC}"
 fi
 
 
+# neofetch
+echo "== NEOFETCH =="
+echo -n " checking for neofetch install .... "
+sleep 1
+if [ $(which neofetch) ]
+then
+    echo -e "${GREEN}INSTALLED${NC}"
+    create_symlink "$PWD/neofetch_config.conf" ~/.config/neofetch/config.conf
+else
+    echo -e "${RED}NOT INSTALLED${NC}"
+fi
+
+
+# Adds all symlinks to the .gitignore
 find . -type l | sed 's/\.\///g' > .gitignore
 
 exit 0
